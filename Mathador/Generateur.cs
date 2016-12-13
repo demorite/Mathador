cd264cb9829e25b1e5be50ff445c6c2380f31028
demorite <dylan3396@hotmail.fr>
2016-12-13 18:30:50 +0100

    Generateur

--- Mathador/Generateur.cs
+++ Mathador/Generateur.cs
@@ -8,5 +8,38 @@ namespace Mathador
 {
     class Generateur
     {
+        private List<int> FirstDice = new List<int>();
+        private List<int> SecondDice = new List<int>();
+        private int TargetNumber;
+        private Random random = new Random();
+
+        public Generateur()
+        {
+            GenerateRandNumber(1, 13, 3, FirstDice);
+            GenerateRandNumber(1, 20, 2, SecondDice);
+            TargetNumber = random.Next(1, 101);
+        }
+
+        private void GenerateRandNumber(int min, int max, int repeat, List<int> liste)
+        {
+            int randNumber;
+
+            for (int i = 0; i < repeat; i++)
+            {
+                randNumber = random.Next(min, max);
+                liste.Add(randNumber);
+            }
+        }
+
+        public override string ToString()
+        {
+            string myGenerator = "";
+            FirstDice.ForEach(x => myGenerator += " "+x);
+            myGenerator +=" / ";
+            SecondDice.ForEach(x => myGenerator += " " + x);
+            myGenerator += " => "+ TargetNumber;
+
+            return myGenerator;
+        }
     }
 }
--- Mathador/InterfaceFront.cs
+++ Mathador/InterfaceFront.cs
@@ -12,25 +12,42 @@ namespace Mathador
 {
     public partial class InterfaceFront : Form
     {
+        private Label generateur;
         private IContainer components;
 
         public InterfaceFront()
         {
             InitializeComponent();
+
+            generateur.Text = new Generateur().ToString();
+
         }
 
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InterfaceFront));
+            this.generateur = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // generateur
+            // 
+            this.generateur.AutoSize = true;
+            this.generateur.Location = new System.Drawing.Point(13, 13);
+            this.generateur.Name = "generateur";
+            this.generateur.Size = new System.Drawing.Size(66, 13);
+            this.generateur.TabIndex = 0;
+            this.generateur.Text = "Generateur :";
             // 
             // InterfaceFront
             // 
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1024, 700);
+            this.Controls.Add(this.generateur);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "InterfaceFront";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
     }
