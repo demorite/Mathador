71728c24602e42f5edf5b1e9a0aa06a7a8e437f2
demorite <dylan3396@hotmail.fr>
2016-12-13 17:41:23 +0100

    ProjectStart !

---
+++ Mathador/InterfaceFront.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mathador
+{
+    public partial class InterfaceFront : Form
+    {
+        private IContainer components;
+
+        public InterfaceFront()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InterfaceFront));
+            this.SuspendLayout();
+            // 
+            // InterfaceFront
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1024, 700);
+            this.Name = "InterfaceFront";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
