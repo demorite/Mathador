7e26c3f8a26a73eaa0f8129b2db5f4c244483a9c
SombreroElGringo <florent.pailhes@hotmail.fr>
2017-01-04 11:34:43 +0100

    Class Solveur Added
    - Class Solveur
    - Function solver

--- Mathador/Solveur.cs
+++ Mathador/Solveur.cs
@@ -8,5 +8,62 @@ namespace Mathador
 {
     class Solveur
     {
+        //Constructor
+        public Solveur()
+        {
+        }
+        
+        /*
+         * @function: solver
+         * @parameter: 
+         *  int[] n -> array of int with the 5 random values
+         *  int r -> result to find
+         * @return: int -> if return 1 mathador hit possible, else return 0
+         */
+        public static int solver(int[] n, int result)
+		{
+			bool loop_lock = true, 
+				find = false;
+			// n_lock = true;
+			int a, r, i = 0, x, z =0;
+
+			for (x = 0; x < 120; x++)
+			{
+				if (!loop_lock)
+				{
+					if (i == 0)
+					{
+						i = n[0];
+						z = Array.IndexOf(n, i);
+
+					}
+
+					if (z < 4)
+					{
+						a = n[z];
+						n[z] = n[z + 1];
+						n[z + 1] = a;
+						z++;
+					}
+					else
+					{
+						i = 0;
+					}
+				}
+
+				r = (((n[0] * n[1]) + n[2]) - n[3]) / n[4];
+
+				Console.WriteLine("try_number_{5} = {0}, {1}, {2}, {3}, {4} result = {6}", n[0], n[1], n[2], n[3], n[4], x, r);
+
+
+				if (r == result)
+					find = true;
+				
+				loop_lock = false;
+			}
+			if (find)
+				return 1;
+			return 0;
+		}
     }
 }
